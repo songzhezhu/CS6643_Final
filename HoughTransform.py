804d65892f27bd8d7c67cd5849c36e8f9805ca29
songzhezhu <songzhezhu@email.arizona.edu>
2019-05-12 19:07:14 -0400

    functionality works now, implement canny and hough transform later

--- HoughTransform.py
+++ HoughTransform.py
@@ -6,25 +6,15 @@ Author: Songzhe Zhu, Xuelin Yu, Yiming Zhang
 
 import cv2
 import numpy as np
+import math
 
 input_img_file = "resize2.png"
 
 
-# 背景处理
-def backgroundSet(img):
-    edge_line = img.copy()
-    corner_point = img.copy()
-    # 高斯模糊
-    img_gauss = cv2.GaussianBlur(img, (3, 3), 0)
-    # 灰度图
-    img_gray = cv2.cvtColor(img_gauss, cv2.COLOR_BGR2GRAY)
-    # 边缘图
-    img_edge = cv2.Canny(img_gray, 50, 250, apertureSize=3)
-    # 画出边缘线
-    lines = cv2.HoughLinesP(img_edge, 1, np.pi/180, 200, minLineLength=20, maxLineGap=10)
-    for x1, y1, x2, y2 in lines[0]:
-        cv2.line(edge_line, (x1, y1), (x2, y2), (0, 0, 255), 1)
-    return img_edge, edge_line
+def canny(img):
+    return 0
+
+
 # 度数转换
 def DegreeTrans(theta):
     res = theta / np.pi * 180
@@ -42,15 +32,21 @@ def rotateImage(src, degree):
     rotate = cv2.warpAffine(src, RotateMatrix, (w, h), borderValue=(0, 0, 0))
     return rotate
 
+def switch_grayscale(image):
+    gray_value = 0.3 * image[:, :, 2] + 0.59 * image[:, :, 1] + 0.11 * image[:, :, 0]
+    gray_img = gray_value.astype(np.uint8)
+    return gray_img
 
 # 通过霍夫变换计算角度
 def CalcDegree(srcImage):
-    midImage = cv2.cvtColor(srcImage, cv2.COLOR_BGR2GRAY)
+    midImage = switch_grayscale(srcImage)
+    #TODO：自己实现canny算子
     dstImage = cv2.Canny(midImage, 50, 200, 3)
     lineimage = srcImage.copy()
 
     # 通过霍夫变换检测直线
     # 第4个参数就是阈值，阈值越大，检测精度越高
+    #TODO: 自己实现霍夫变换
     lines = cv2.HoughLines(dstImage, 1, np.pi / 180, 200)
     # 由于图像不同，阈值不好设定，因为阈值设定过高导致无法检测直线，阈值过低直线太多，速度很慢
     sum = 0
@@ -69,23 +65,19 @@ def CalcDegree(srcImage):
             # 只选角度最小的作为旋转角度
             sum += theta
             cv2.line(lineimage, (x1, y1), (x2, y2), (0, 0, 255), 1, cv2.LINE_AA)
-            cv2.imshow("Imagelines", lineimage)
 
     # 对所有角度求平均，这样做旋转效果会更好
     average = sum / len(lines)
     angle = DegreeTrans(average) - 90
-    return angle
+    return angle, lineimage, dstImage
 
 
 if __name__ == '__main__':
     image = cv2.imread(input_img_file)
-    cv2.imshow("Image", image)
-    # 背景处理
-    img_edge, img_line = backgroundSet(image)
-    cv2.imwrite('edge.png', img_edge)
-    cv2.imwrite('lines.png', img_line)
     # 倾斜角度矫正
-    degree = CalcDegree(image)
+    degree, img_edge, canny = CalcDegree(image)
     print("调整角度：", degree)
     rotate = rotateImage(image, degree)
     cv2.imwrite("rotate.png", rotate)
+    cv2.imwrite('edge.png', img_edge)
+    cv2.imwrite('canny.png', canny)
